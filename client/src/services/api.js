00f6ea84fbe8f0afda8c779417079e4d22b45317
RapuruHitheshReddy <rapuruhitheshreddy@gmail.com>
2025-07-08 16:34:18 +0530

    Fix API base URL and push production build for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,8 @@
-// client/utils/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_BASE || 'http://localhost:5000',
   withCredentials: true, // ✅ Include cookies in requests
 });
-
-
 
 export default api;
